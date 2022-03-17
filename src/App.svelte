6ae30b298e5778aa20421764fe8a86aa8a0ba29b
1zumisawashun <shun.1zumisawa@gmail.com>
2022-03-17 10:43:59 +0900

    投票を増減させる機能の追加

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,66 @@
 <script>
-	import Header from "./components/Header.svelte";
-	import Footer from "./components/Footer.svelte";
-	import Tabs from "./shared/Tabs.svelte";
-	import CreatePollForm from "./components/CreatePollForm.svelte";
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import PollList from "./components/PollList.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  import CreatePollForm from "./components/CreatePollForm.svelte";
 
-	//tabs
-	let items = ["Current Polls", "Add New Polls"];
-	let activeItem = "Current Polls";
+  //tabs
+  let items = ["Current Polls", "Add New Polls"];
+  let activeItem = "Current Polls";
 
-	const tabChange = (e) => {
-		activeItem = e.detail;
-	};
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
+  //polls
+  let polls = [
+    {
+      id: 1,
+      question: "Python or JavaScript?",
+      answerA: "Python",
+      answerB: "JavaScript",
+      votesA: 9,
+      votesB: 15,
+    },
+  ];
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    console.log(polls);
+    activeItem = "Current Polls";
+  };
+
+  const handleVote = (e) => {
+    const { id, option } = e.detail;
+    let copiedPolls = [...polls];
+    let upvotedPoll = copiedPolls.find((poll) => {
+      return poll.id === id;
+    });
+    if (option === "a") {
+      upvotedPoll.votesA++;
+    }
+    if (option === "b") {
+      upvotedPoll.votesB++;
+    }
+    polls = copiedPolls;
+  };
 </script>
-
-<style>
-	main {
-		max-width: 960px;
-		margin: 40px auto;
-	}
-</style>
 
 <Header />
 <main>
-	<Tabs {items} {activeItem} on:tabChange={tabChange} />
-	{#if activeItem === 'Current Polls'}
-		<p>Poll List Component goes here</p>
-	{:else if activeItem === 'Add New Polls'}
-		<CreatePollForm />
-	{/if}
+  <Tabs items="{items}" activeItem="{activeItem}" on:tabChange="{tabChange}" />
+  {#if activeItem === "Current Polls"}
+    <!-- NOTE:{polls}に省略することができる。がフォーマッターで省略できないようにしている -->
+    <PollList polls="{polls}" on:vote="{handleVote}" />
+  {:else if activeItem === "Add New Polls"}
+    <CreatePollForm on:add="{handleAdd}" />
+  {/if}
 </main>
 <Footer />
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
